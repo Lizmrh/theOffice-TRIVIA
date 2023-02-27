4595bd2b59bc0ae6bb55ca2af78538f33c7e47b5
Lizmrh <elizabethfruciano@gmail.com>
2023-02-26 20:36:24 -0500

    project 1 phase one

---
+++ README.md
@@ -0,0 +1,15 @@
+USER EXPERIENCE
+
+1. A question will appear with two possible answers. Each player begins with 3 freebies
+2. When the correct is clicked, the game will prompt the next question and the player will accumulate X points
+3. The player has X amount of seconds to respond and the answer clicked will turn green
+4. When the player wins all the answers, image appears saying YOU ARE THE WINNER.... Or some prompt that says you are the winner, hired as assistant to the assistant of the regional manager
+5. When the incorrect answer is clicked the answer box will turn red and player will lose 1 freebie. 
+When all freebies have been exhausted, there will be a GAME OVER prompt or image
+
+
+Hungry for More
+1. Game instructions and START button
+2. Help features for a question (stop time or get clue or skip question)
+3. Making difficulty levels with points
+
---
+++ trivia.js
@@ -0,0 +1,26 @@
+questions = [
+    "Pam and Jim's first kiss took place where?",
+    "Where does Michael Scott move to start his new life with Holly?",
+    "What are the names of Jim and Pam Halpert's kids?",
+    "Who was on the jury for the Scranton Strangler case?",
+    "Which of Angela's cats did Dwight kill?",
+    "Who was Pam engaged to before Jim?",
+    "Michael and Dwight tried to steal clients from which local competing business?",
+    "How many brothers does Jim Halpert have?",
+    "How much does Bob Vance bid on a hug from his wife Phyllis?",
+    "Who did Michael end up taking to Jamaica?",
+]
+// console.log(questions)
+answers = [
+    ["Chili's", "Applebee's"],
+    ["Boulder, Colorado", "Phoenix, Arizona"],
+    ["Cece and Phillip", "Penelope and Will"],
+    ["Toby", "Kelly"],
+    ["Sprinkles", "Cinnamon"],
+    ["Roy", "Rob"],
+    ["Prince Family Paper", "Royal Group Paper Supplies"],
+    ["Two", "One"],
+    ["$1000", "$585"],
+    ["Jan", "Holly"],
+]
+console.log(answers)
