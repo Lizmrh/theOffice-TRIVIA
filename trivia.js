484c43d913dabc9cce7f7c7f13e3695ecb2ca431
Lizmrh <elizabethfruciano@gmail.com>
2023-02-26 21:10:09 -0500

    JS updated into class

--- trivia.js
+++ trivia.js
@@ -1,26 +1,19 @@
-questions = [
-    "Pam and Jim's first kiss took place where?",
-    "Where does Michael Scott move to start his new life with Holly?",
-    "What are the names of Jim and Pam Halpert's kids?",
-    "Who was on the jury for the Scranton Strangler case?",
-    "Which of Angela's cats did Dwight kill?",
-    "Who was Pam engaged to before Jim?",
-    "Michael and Dwight tried to steal clients from which local competing business?",
-    "How many brothers does Jim Halpert have?",
-    "How much does Bob Vance bid on a hug from his wife Phyllis?",
-    "Who did Michael end up taking to Jamaica?",
-]
-// console.log(questions)
-answers = [
-    ["Chili's", "Applebee's"],
-    ["Boulder, Colorado", "Phoenix, Arizona"],
-    ["Cece and Phillip", "Penelope and Will"],
-    ["Toby", "Kelly"],
-    ["Sprinkles", "Cinnamon"],
-    ["Roy", "Rob"],
-    ["Prince Family Paper", "Royal Group Paper Supplies"],
-    ["Two", "One"],
-    ["$1000", "$585"],
-    ["Jan", "Holly"],
-]
-console.log(answers)
+class Trivia {
+    constructor (name, questions, trueAnswer, falseAnswer) {
+        this.name = name;
+        this.questions = questions;
+        this.trueAnswer = trueAnswer;
+        this.falseAnswer = falseAnswer
+    }
+}
+
+const Trivia1 = new Trivia ("question1", "Pam and Jim's first kiss took place where?", "Chili's", "Applebee's")
+const Trivia2 = new Trivia ("question2", "Where does Michael Scott move to start his new life with Holly?", "Boulder, Colorado", "Phoenix, Arizona")
+const Trivia3 = new Trivia ("question3", "What are the names of Jim and Pam Halpert's kids?", "Cece and Phillip", "Penelope and Will")
+const Trivia4 = new Trivia ("question4", "Who was on the jury for the Scranton Strangler case?", "Toby", "Kelly")
+const Trivia5 = new Trivia ("question5", "Which of Angela's cats did Dwight kill?", "Sprinkles", "Cinnamon")
+const Trivia6 = new Trivia ("question6", "Who was Pam engaged to before Jim?", "Roy", "Rob")
+const Trivia7 = new Trivia ("question7", "Michael and Dwight tried to steal clients from which local competing business?", "Prince Family Paper", "Royal Group Paper Supplies")
+const Trivia8 = new Trivia ("question8", "How many brothers does Jim Halpert have?","Two", "One")
+const Trivia9 = new Trivia ("question9", "How much does Bob Vance bid on a hug from his wife Phyllis?","$1000", "$585")
+const Trivia10 = new Trivia ("question10", "Who did Michael end up taking to Jamaica?","Jan", "Holly")
